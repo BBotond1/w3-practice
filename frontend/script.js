5a47fbf96875e9ae239576dd7653693e4554553c
Errorcode07 <bbotond0829@gmail.com>
2022-09-14 15:51:35 +0300

    new script

--- frontend/script.js
+++ frontend/script.js
@@ -1,19 +1,34 @@
 let myVariable = "value";
-console.log(myVariable);
-console.log(typeof myVariable);
+// console.log(myVariable);
+// console.log(typeof myVariable);
 
-myVariable=1;
-console.log(myVariable);
-console.log(typeof myVariable);
+myVariable = 1;
+// console.log(myVariable);
+// console.log(typeof myVariable);
 
-myVariable=true;
-console.log(myVariable);
-console.log(typeof myVariable);
+myVariable = true;
+// console.log(myVariable);
+// console.log(typeof myVariable);
 
-let myVariable2= "szoveg";
-myVariable=myVariable2;
-console.log("myvariable2= ",myVariable2);
+let myVariable2 = "szoveg";
+myVariable = myVariable2;
+// console.log("myvariable2= ", myVariable2);
 
-let myVariable3=myVariable2+myVariable;
-console.log("myvariable3= ",myVariable3);
+let myVariable3 = myVariable2 + myVariable;
+// console.log("myvariable3= ", myVariable3);
 
+function sumVariables(Variable1,Variable2) {
+    console.log(Variable2 + Variable1)
+}
+
+sumVariables("word","car");
+sumVariables(8,3);
+sumVariables(12,"89");
+
+function loadEvenet () {
+    console.log ("loaded");
+    let rootElement = document.getElementById("root");
+    rootElement.insertAdjacentHTML ("beforeend", "Hello world");
+}
+
+window.addEventListener ("click", loadEvenet);
