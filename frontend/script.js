23c478557c4726f2f7aa5d101de95e5654551819
Errorcode07 <bbotond0829@gmail.com>
2022-09-14 12:43:26 +0300

    javascript

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+let myVariable = "value";
+console.log(myVariable);
+console.log(typeof myVariable);
+
+myVariable=1;
+console.log(myVariable);
+console.log(typeof myVariable);
+
+myVariable=true;
+console.log(myVariable);
+console.log(typeof myVariable);
+
+let myVariable2= "szoveg";
+myVariable=myVariable2;
+console.log("myvariable2= ",myVariable2);
+
+let myVariable3=myVariable2+myVariable;
+console.log("myvariable3= ",myVariable3);
+
